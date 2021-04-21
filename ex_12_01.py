f939a2c00fadc738641b5104397b152409ffa487
Jkearns138 <50673111+Jkearns138@users.noreply.github.com>
2021-04-21 16:05:20 -0400

    Add files via upload

---
+++ ex_12_01.py
@@ -0,0 +1,16 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  4 2021, 13:27:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+import socket
+
+mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.enconde()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
